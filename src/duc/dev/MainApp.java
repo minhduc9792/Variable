e6f274ce13ea1d20fab7d45ea98e841741c8ed7f
duc <tonyngusi0810@gmail.com>
2021-11-01 14:15:44 +0700

    first commit

---
+++ src/duc/dev/MainApp.java
@@ -0,0 +1,23 @@
+package duc.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String fistName = "Phan Quang Minh";
+        String lastName = "Duc";
+        int old = 21;
+        float markMath = 9f;
+        boolean passed = true;
+        
+        System.out.println("Ho va ten: " + fistName +" "+ lastName);
+        System.out.println("Mark Math: " + markMath);
+        System.out.println("Passed: " + passed);
+        
+        final float PI = 3.14f;
+        
+        
+        
+	}
+
+}
